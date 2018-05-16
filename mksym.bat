de63531cfe937694c92624daa2aeba7afe68fd03
myunpe <myunpe2@gmail.com>
2018-05-17 01:50:05 +0900

    シンボリックリンク作成ツール修正

--- mksym.bat
+++ mksym.bat
@@ -1,5 +1,14 @@
 cd /d %~dp0
 
-rmdir "Assets\Plugins\UniRx\Scripts"
-mklink /D "Assets\Plugins\UniRx\Scripts" "submodules\UniRx\Assets\Plugins\UniRx\Scripts"
+cd Assets\Plugins\UniRx\
+rm Scripts
+mklink /D Scripts ..\..\..\submodules\UniRx\Assets\Plugins\UniRx\Scripts
 
+cd /d %~dp0
+
+cd "Assets\LINQtoGameObject"
+rm Scripts
+mklink /D Scripts "..\..\submodules\LINQ-to-GameObject-for-Unity\Assets\LINQtoGameObject\Scripts"
+
+cd /d %~dp0
+pause
