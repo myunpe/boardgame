c000327bba76dae2844093a2c933cf995e405fb1
myunpe <myunpe2@gmail.com>
2018-06-07 08:08:20 +0900

    ゲームシーンとか追加

---
+++ Assets/Scripts/DEF_Scene.cs
@@ -0,0 +1,7 @@
+﻿namespace DEF {
+public static class S { 
+	public const string GAME = "game";
+	public const string SAMPLESCENE = "SampleScene";
+	public const string STARTUP = "startup";
+}
+}
---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class Startup : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MYSceneManager.Instance.StartSceneAsync(DEF.S.SAMPLESCENE).StartAsCoroutine();
+	}
+
+}
